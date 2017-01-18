1ece034e294bf5829700d0d8687f790016614813
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-01-18 15:32:58 -0500

    correct property test name

--- javascript/kth-element.spec.js
+++ javascript/kth-element.spec.js
@@ -15,7 +15,7 @@ describe('Kth Element in Linear Time', function() {
     expect(KthElement.find([2,4,1,0,9], 1)).to.equal(0);
   });
   describe("find has cheat's correctness", function () {
-    jsc.property("idempotent", "array nat", function (arr) {
+    jsc.property("cheat == find", "array nat", function (arr) {
       return KthElement.find(arr, 2) === KthElement.cheat(arr, 2);
     });
   });
