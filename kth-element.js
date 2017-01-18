d60a1f8c25d1471d2205275944400bc247face03
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-01-18 13:14:56 -0500

    property test

--- kth-element.js
+++ kth-element.js
@@ -1,3 +1,9 @@
+// KthElement in this case means Kth order statistic
+// for example, in an array like [33,4,22,0,9,5],
+// the 1st order statistic (if we're starting at 1) is 0
+// the 2nd order statistic would be 4, 3rd is 5, & so on
+// but our goal is to do better than the cheat function
+// while still maintaining its correctness
 var KthElement;
 
 KthElement = {
--- kth-element.spec.js
+++ kth-element.spec.js
@@ -1,14 +1,22 @@
 'use strict';
 
 var expect = require('chai').expect;
+var jsc = require("jsverify");
+var KthElement = require('./kth-element.js');
 
 describe('Kth Element in Linear Time', function() {
   it('should exist', function() {
-    var KthElement = require('./kth-element.js');
     expect(KthElement).to.not.be.undefined;
   });
   it('provides a working O(n log n) implementation using sort', function() {
-    var KthElement = require('./kth-element.js');
     expect(KthElement.cheat([2,4,1,0,9], 1)).to.equal(0);
+  });
+  it('provides a working O(n) implementation without sort', function() {
+    expect(KthElement.find([2,4,1,0,9], 1)).to.equal(0);
+  });
+  describe("find has cheat's correctness", function () {
+    jsc.property("idempotent", "array nat", function (arr) {
+      return KthElement.find(arr, 2) === KthElement.cheat(arr, 2);
+    });
   });
 });
